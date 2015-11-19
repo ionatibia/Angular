313b706ffed47b75a7df0dddc53df7e95b725500
natiexperiencia <natiexperiencia@gmail.com>
2015-11-19 14:02:38 +0100

    mas

--- DW32/routes-igor/angulardw32igor-master/js/controllers.js
+++ DW32/routes-igor/angulardw32igor-master/js/controllers.js
@@ -20,9 +20,10 @@ amigosControllers.controller('amigosCtrl', ['$scope','$rootScope',
   }]);
 amigosControllers.controller('amigoEditCtrl', ['$scope','$rootScope', '$routeParams',
   function($scope,$rootScope,$routeParams) {
-  	for(i=0;i<$rootScope.amigos.length;i++){
+  	/*for(i=0;i<$rootScope.amigos.length;i++){
   		if ($rootScope.amigos[i].nombre == $routeParams.amigoId) {
   			$scope.amigo=$rootScope.amigos[i];
   		}
-  	}
+  	}*/
+  	$scope.amigo = $rootScope.amigos[$routeParams.amigoId];//muestra el indice en la url
   }]);
