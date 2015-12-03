c0edc1f69e0936c3a31558878cd866e8198a261b
natiexperiencia <natiexperiencia@gmail.com>
2015-12-03 11:04:37 +0100

    modal

--- DW32/3-Routes-igor & factory/js/controllers.js
+++ DW32/3-Routes-igor & factory/js/controllers.js
@@ -1,25 +1,6 @@
 var amigosControllers = angular.module('amigosControllers', ['mgcrea.ngStrap','ngAnimate']);
 
 //controlador para el index
-/*amigosControllers.controller('appCtrl', ['$scope','$rootScope','$location', function($scope,$rootScope,$location){
-	$rootScope.amigos = [
-	{
-		nombre:"juan",
-		tlfno:"123456789"
-	},
-	{
-		nombre:"pedro",
-		tlfno:"123456789"
-	},	
-	{
-		nombre:"luis",
-		tlfno:"123456789"
-	}
-	];
-	$scope.isActive=function(viewLocation) {
-		return viewLocation === $location.path();
-	};
-}]);*/
 amigosControllers.controller('appCtrl',function ($scope,$rootScope,$location) {
 	//devolver class active segun scope
 	$scope.isActive = function (viewLocation) {
@@ -30,23 +11,14 @@ amigosControllers.controller('appCtrl',function ($scope,$rootScope,$location) {
 
 
 //controlador para la lista de amigos
-/*amigosControllers.controller('amigosCtrl', ['$scope','$rootScope', 
-  function($scope,$rootScope) {
-    
-	//$scope.amigos=$rootScope.amigos;
-  }]);*/
 amigosControllers.controller('amigosCtrl',['$scope','amigoSrv',
 		function ($scope,amigoSrv) {
 			$scope.amigos = amigoSrv.get();
 		}]);
 
 //controlador para la edicion de amigos
-/*amigosControllers.controller('amigoEditCtrl', ['$scope','$rootScope', '$routeParams',
-  function($scope,$rootScope,$routeParams) {
-  		$scope.amigo = $rootScope.amigos[$routeParams.amigoId];//muestra el indice en la url
-  }]);*/
-amigosControllers.controller('amigoEditCtrl',['$scope','$rootScope','$routeParams','amigoSrv',
-		function ($scope,$rootScope,$routeParams,amigoSrv) {
+amigosControllers.controller('amigoEditCtrl',['$scope','$rootScope','$routeParams','amigoSrv', '$modal',
+		function ($scope,$rootScope,$routeParams,amigoSrv,$modal) {
 			$scope.amigo = amigoSrv.find($routeParams.amigoId);
 
 			$scope.guardar=function () {
@@ -55,12 +27,22 @@ amigosControllers.controller('amigoEditCtrl',['$scope','$rootScope','$routeParam
 			}
 			$scope.eliminar=function () {
 				$scope.amigo = amigoSrv.delete($routeParams.amigoId);
+/*<<<<<<<<<<<<<<<<Desde aqui>>>>>>>>>>>>>>>>*/
+				$scope.myModal.hide();
+				//myModal.hide();
 			}
-			$scope.modal = {
-			  "title": "Title",
-			  "content": "Hello Modal<br />This is a multiline message!"
+
+			$scope.myModal = $modal({ scope: $scope, templateUrl:'modal.html', show:false});
+			$scope.askEliminar=function () {
+				//var myModal = $modal({scope: $scope, templateUrl:'modal.html', show:true});
+				$scope.myModal.show();
 			};
+			/*$scope.modal = {
+			  "title": "Seguro???",
+			  "content": "Quieres borrar al amigo "+$scope.amigo.nombre
+			};*/
 		}]);
+/*<<<<<<<<<<<<<<<<Hasta aqui>>>>>>>>>>>>>>>>*/
 
 amigosControllers.controller('amigoNewCtrl', ['$scope','$rootScope','$routeParams', 'amigoSrv',
 	function($scope,$rootScope,$routeParams,amigoSrv){
@@ -68,7 +50,6 @@ amigosControllers.controller('amigoNewCtrl', ['$scope','$rootScope','$routeParam
 		$scope.guardar=function() {
 			if ($scope.amigo.nombre!="") {
 				amigoSrv.add($scope.amigo);
-				//$rootScope.amigos.push($scope.amigo);
 			}
 		}
 }]);
