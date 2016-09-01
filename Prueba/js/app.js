950cf170dc7614ded55ec79ccd854a7eb6e02977
Iker Oñatibia Salvador <ionatibia@s21sec.int>
2016-09-01 14:21:15 +0200

    navigation

--- Prueba/js/app.js
+++ Prueba/js/app.js
@@ -1,8 +1,34 @@
-angular.module('app',[])
+angular.module('app',["ngRoute"])
+	.config(function($routeProvider) {
+	    $routeProvider
+	    .when("/", {
+	        templateUrl : "../views/main.html"
+	    })
+	    .when("/dispensa", {
+	        templateUrl : "../views/dispensa.html"
+	    })
+	})
 	.controller('AppCtrl', ['$scope','$location', function ($scope,$location) {
+
 		//Active links
 		$scope.isActive = function (viewLocation) { 
         	return viewLocation === $location.path();
     	};
-		$scope.loginOk = true;
-	}])
+
+    	//Login comprobation
+		$scope.checkLogin = function () {
+			/*if ($scope.token) {
+				return true;
+			} else {
+				return false;
+			}*/
+			return true
+		}//check login
+
+		//Collapse menu on hamurguer form
+		$(function () {
+            $('.navbar-collapse ul li a:not(.dropdown-toggle)').bind('click touchstart', function () {
+                    $('.navbar-toggle:visible').click();
+            });
+    	});
+	}])//AppCtrl controller
